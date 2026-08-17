7de92ddab1b351af9bcbdf3729b7722fb1cb337b
Amirtha Ganesh <amirthaganeshramesh@gmail.com>
2026-08-17 09:51:24 +0530

    pydantic models ready

--- diet_agent/main.py
+++ diet_agent/main.py
@@ -1,4 +1,4 @@
-from models import UserProfile, Goal , WorkoutTypes
+from models import Focus, UserProfile, Goal , WorkoutType , DietType , FoodEntry , WorkoutEntry
 
 user = UserProfile(
     name="Ganesh",
@@ -7,9 +7,26 @@ user = UserProfile(
     height_cm=178,
     weight_kg=74,
     goal= Goal.GENERAL_FITNESS,
-    workout_type=WorkoutTypes.GYM,
+    workout_type=WorkoutType.GYM,
     diet_type="Non-vegetarian"
 )
 
 print(user)
 
+food = FoodEntry(
+    food_name="Egg",
+    quantity=2,
+    unit="pieces",
+    time="08:30 Am"
+)
+
+print(food)
+
+Workout = WorkoutEntry(
+    focus=Focus.CHEST,
+    duration=45,
+    unit="minutes",
+    time="06:30 Am"
+)
+
+print(Workout)
--- diet_agent/models.py
+++ diet_agent/models.py
@@ -11,7 +11,7 @@ class Goal(str, Enum):   # Goal -> the enum class,  (str,enum) -> base/parent cl
     GENERAL_FITNESS = "General fitness"
 
 
-class WorkoutType(str,Enum):    # 
+class WorkoutType(str,Enum):    
     """ An enumeration representing different workout types."""
     GYM = "Gym"
     HOME_WORKOUT = "Home workout"
@@ -24,7 +24,34 @@ class WorkoutType(str,Enum):    #
     OTHER = "Other"
     NONE = "No workout"
     
+class DietType(str,Enum):
+    """ An enumeration representing different diet types."""
+    VEGETARIAN = "Vegetarian"
+    NON_VEGETARIAN = "Non-vegetarian"
+    VEGAN = "Vegan"
+    EGGETARIAN = "Eggetarian"
+    PESCATARIAN = "Pescatarian"
+    OTHER = "Other"
 
+class Focus(str, Enum):
+    """An enumeration representing different training focus areas."""
+    FULL_BODY = "Full body"
+    UPPER_BODY = "Upper body"
+    LOWER_BODY = "Lower body"
+    CHEST = "Chest"
+    BACK = "Back"
+    SHOULDERS = "Shoulders"
+    BICEPS = "Biceps"
+    TRICEPS = "Triceps"
+    LEGS = "Legs"
+    CORE = "Core"
+    GLUTES = "Glutes"
+    CARDIO = "Cardio"
+    ENDURANCE = "Endurance"
+    FLEXIBILITY = "Flexibility"
+    MOBILITY = "Mobility"
+    OTHER = "Other"
+  
 
 class UserProfile(BaseModel):
     """A class representing a user's profile information."""
@@ -34,6 +61,20 @@ class UserProfile(BaseModel):
     height_cm:float
     weight_kg:float
     goal:Goal
-    workout_type:WorkoutTypes
-    diet_type:str
+    workout_type:WorkoutType
+    diet_type:DietType
     
+class FoodEntry(BaseModel):
+    """A class representing a food entry with its food name, quantity and time ."""
+    food_name:str
+    quantity:float
+    unit:str
+    time:str
+
+
+class WorkoutEntry(BaseModel):
+    """A class representing a workout entry with its workout type, duration and time."""
+    focus:Focus
+    duration:int
+    unit:str
+    time:str 
